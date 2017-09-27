f7474e35da796644c97ae66025c0fb9664777d1d
AbdallahEid <abdallaheidm@gmail.com>
2017-09-27 21:35:14 +0200

    add logic to the application

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,10 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //variables
+    
+    var timer : Timer!
+    
+    var cnt = 210
+    
+    var flip = true
 
+    // layout connection
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        
+        timer.invalidate()
+        
+        flip = false
+        
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        
+        if !flip {
+            
+            runTimer()
+            
+        }
+        
+        flip = true
+        
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+        cnt = 210
+      
+    }
+    
+    @IBAction func plus10Button(_ sender: Any) {
+        
+        cnt += 10
+        
+    }
+    
+    @IBAction func minus10Button(_ sender: Any) {
+        
+        if cnt < 10 {
+            
+            cnt = 0
+            
+        }else {
+            
+            cnt -= 10
+            
+        }
+        
+    }
+    
+    // functions
+    
+    func processingTimer(){
+        
+        if cnt < 0 {
+            
+            timer.invalidate()
+            
+            flip = false
+            
+        }
+        else {
+        
+            timerLabel.text = "\(cnt)"
+        
+            cnt -= 1
+        }
+        
+    }
+    
+    func runTimer(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processingTimer), userInfo: nil, repeats: true)
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        runTimer()
+        
     }
 
     override func didReceiveMemoryWarning() {
